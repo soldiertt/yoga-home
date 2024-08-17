7a3d3a8ccfb2e45cc3ac8073716df023a4d49ad6
soldiertt <soldiertt@gmail.com>
2024-08-17 14:30:11 +0200

    upgrade angular + maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {catchError, map, Observable, of} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  apiLoaded: Observable<boolean>;
   title = 'yoga-home';
+
+
+  constructor(httpClient: HttpClient) {
+    this.apiLoaded = httpClient
+      .jsonp(
+        'https://maps.googleapis.com/maps/api/js?key=AIzaSyBMV0h2icKO2DcFhVHdPLZ5Sgkws5wxcEU',
+        'initMap'
+      )
+      .pipe(
+        map(() => true),
+        catchError(() => of(false))
+      );
+  }
 }
